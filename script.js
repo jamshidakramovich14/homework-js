3e7b10c476be1ab9159a9af08be5932faa33b85b
jamshidakramovich14 <jamshidturdiyev103>
2024-01-14 22:33:03 +0500

    all files

---
+++ script.js
@@ -0,0 +1,253 @@
+// 3- masala
+
+// let a = prompt("Sonni kiriting:")
+
+// if(Number.isInteger(a) && a % 2 == 0){
+//     console.log("True");
+// }else{
+//     console.log("False");
+// }
+
+// 4- masala
+
+// let a = +prompt("a Sonni kiriting:");
+// let b = +prompt("b Sonni kiriting:");
+
+// if(Number.isInteger(a) && Number.isInteger(b) && a > 0 && b > 0){
+//     console.log("True");
+// }else{
+//     console.log("False");
+// }
+
+// 5- msala
+
+// let a = +prompt(" a Sonini kiriting:");
+// let b = +prompt(" b Sonini kiriting:");
+// let c = +prompt(" c Sonini kiriting:");
+
+// if(a > b && b > c){
+//     console.log("True");
+// }else{
+//     console.log("False");
+// }
+
+// 6- masala
+
+// let a = +prompt(" a Sonini kiriting:");
+// let b = +prompt(" b Sonini kiriting:");
+// let c = +prompt(" c Sonini kiriting:");
+
+// if ((a > b && b > c) && (a < b && b < c)) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// 7- masala
+
+// let a = prompt("a Sonini kiriting:");
+// let b = prompt("b Sonini kiriting:");
+
+// if (a % 2 == 1 && b % 2 == 1){
+//     console.log("True");
+// }else{
+//     console.log("False");
+// }
+
+// 8- masala
+
+// let a = prompt("a Sonini kiriting:");
+// let b = prompt("b Sonini kiriting:");
+// let c = prompt("c Sonini kiriting:");
+// let d = prompt("d Sonini kiriting:");
+
+// if(a > 0 && b > 0 && c > 0 && d > 0 ){
+//     console.log("True");
+// }else{
+//     console.log("True");
+// }
+
+// 9- masala
+// let a = +prompt("a Sonini kiriting:");
+// let b = +prompt("b Sonini kiriting:");
+// let c = +prompt("c Sonini kiriting:");
+
+// if((a > 0 && b > 0 && c > 0) || (a > 0 && b > 0 && c < 0) || (a > 0 && b < 0 && c >0) || (a < 0 && b > 0 && c > 0)){
+//     console.log("True");
+// }else{
+//     console.log("False");
+// }
+
+// 10- masala
+
+// let kun = prompt("Sonni kiriting:");
+// if(kun == 1){
+//     document.body.innerHTML = "Dushanba"
+// }else if(kun == 2){
+//     document.body.innerHTML = "Seshanba"
+// }else if(kun == 3){
+//     document.body.innerHTML = "Chorshanba"
+// }else if(kun == 4){
+//     document.body.innerHTML = "Payshanba"
+// }else if(kun == 5){
+//     document.body.innerHTML = "Juma"
+// }else if(kun == 6){
+//     document.body.innerHTML = "Shanba"
+// }else if(kun == 7){
+//     document.body.innerHTML = "Yakshanba"
+// }else{
+//     document.body.innerHTML = "Bunday hafta kuni yo'q"
+// }
+
+// 11- masala
+
+// let phone = prompt("Telefon raqamingizni kiriting:");
+
+// let phoneCode = phone[0] + phone[1];
+
+// if(phoneCode === "99"){
+//     document.body.innerHTML = `Uzmobile nomer: $ {phone}`;
+// }else if(phoneCode === "97"){
+//     document.body.innerHTML = `Mobiuz nomer: ${ phone}`;
+// }else if(phoneCode === "94"){
+//     document.body.innerHTML = `Usell nomer: ${ phone}`;
+// }else if(phoneCode === "90"){
+//     document.body.innerHTML = `Beeline nomer: ${ phone}`;
+// }
+
+// 12- masala
+
+// let a = +prompt("Sonni kiriting:");
+
+// if(a > 0){
+//     document.body.innerHTML = a + 1;
+// }else if(a < 0){
+//     document.body.innerHTML = a + 1;
+// }
+
+// 13- masala
+
+// let a = +prompt("Sonni kiriting:");
+
+// if(a > 0){
+//     document.body.innerHTML = a + 3;
+// }else if(a < 0){
+//     document.body.innerHTML = a + 2;
+// }
+
+// 14- masala
+
+// let a = prompt("a Sonini kiriting:");
+// let b = prompt("b Sonini kiriting:");
+
+// document.body.innerHTML = Math.max(a, b);
+
+// 15- masala
+
+// let a = prompt("a Sonini kiriting:");
+// let b = prompt("b Sonini kiriting:");
+// let c = prompt("c Sonini kiriting:");
+
+// document.body.innerHTML = Math.max(a, b, c);
+
+// 16- masala
+
+// let a = prompt("a Sonini kiriting:");
+// let b = prompt("b Sonini kiriting:");
+// let c = prompt("c Sonini kiriting:");
+
+// document.body.innerHTML = Math.min(a, b, c);
+
+// 18- masala
+
+// let baho = prompt("Bahoni kiriting:");
+
+// if(baho == 5){
+//     document.body.innerHTML = `1.600.000 so'm`
+// }else if(baho == 4){
+//     document.body.innerHTML = `1.200.000 so'm`
+// }
+// else if(baho == 3){
+//     document.body.innerHTML = `1.000.000 so'm`
+// }else{
+//     document.body.innerHTML = `0 so'm`
+// }
+
+// 19- masala
+
+// let n = prompt("Sonni kitiing:");
+
+// if(n > 0 && n % 2 == 0){
+//     document.body.innerHTML = `Musbat juft`;
+// }else if(n > 0 && n % 2 == 1){
+//     document.body.innerHTML = `Musbat toq`;
+// }else if(n < 0 && n % 2 == 0){
+//     document.body.innerHTML = `Manfiy juft`;
+// }else{
+//     document.body.innerHTML = `Manfiy toq`;
+// }
+
+// 20- masala
+
+// let yil = prompt("Tug'ilgan yilingizni kiriting: ");
+
+// document.body.innerHTML =   2024 - yil ;
+
+//  21- masala
+
+// let a = prompt("a Sonini kiriting:");
+// let b = prompt("b Sonini kiriting:");
+// let c = prompt("c Sonini kiriting:");
+
+// if(a <= b && b <= c){
+//     document.body.innerHTML = "True";
+// }else{
+//     document.body.innerHTML = "False";
+// }
+
+//  22- masala
+
+// let a = prompt("a Sonini kiriting:");
+// let b = prompt("b Sonini kiriting:");
+
+// if((a % 2 == 0 && b % 2 == 1)  || (a % 2 == 1 && b % 2 == 0)){
+//     document.body.innerHTML = "True";
+// }else{
+//     document.body.innerHTML = "False";
+// }
+
+//  23- masala
+
+// let a = prompt("a Sonini kiriting:");
+// let b = prompt("b Sonini kiriting:");
+// let c = prompt("c Sonini kiriting:");
+
+// if(a > 0 && b > 0 && c > 0){
+//     document.body.innerHTML = "True";
+// }else{
+//     document.body.innerHTML = "False";
+// }
+
+//  24- masala
+
+// let a = prompt("a Sonini kiriting:");
+// let b = prompt("b Sonini kiriting:");
+// let c = prompt("c Sonini kiriting:");
+
+// if((a < 0 && b < 0 && c > 0)  || (a < 0 && b > 0 && c < 0) || (a > 0 && b < 0 && c < 0)){
+//     document.body.innerHTML = "True";
+// }else{
+//     document.body.innerHTML = "False";
+// }
+// 26- masala
+
+// let komp = prompt("Kompyuter nomini kiriting:");
+// if(komp == "Macbook"){
+//     document.body.innerHTML = `Narxi $1200`;
+// }else if(komp == "HP"){
+//     document.body.innerHTML = `Narxi $500`;
+// }else if(komp == "Aser"){
+//     document.body.innerHTML = `Narxi $400`;
+// }else{
+//     document.body.innerHTML = `Bunday noutbuk yo'q`
+// }
